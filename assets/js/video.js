e489fcfe4faf5b54248731e0c2217f30d2cb34e4
remidev <28935481+remidev@users.noreply.github.com>
2018-11-20 18:25:16 -0500

    last minute changes

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,3 +1,6 @@
+var globalVideoId = "BsY0Tazygqk";
+
+//Function to query youtube api, search for video with query "emotion + music"
 function videoQuery(emotion) {
 
   //Query object for URL parameters
@@ -19,9 +22,10 @@ function videoQuery(emotion) {
   //Append parameters to base URL
   var queryURL = baseURL + jQuery.param(query);
 
+  //Log query URL
   console.log("Query URL: " + queryURL);
 
-
+  //ajax GET request 
   $.ajax({
     url: queryURL,
     method: "GET",
@@ -30,8 +34,9 @@ function videoQuery(emotion) {
     success: function (response) {
 
       //retrieve video ID from response object
-      videoId = response.items[0].id.videoId;
-      //Set Iframe video ID 
+      globalVideoId = response.items[0].id.videoId;
+      //find a way to update player videoId
+      // call some function declared below the player object?
 
     },
 
@@ -42,10 +47,70 @@ function videoQuery(emotion) {
       getDadJoke();
 
     }
-  })
+  });
 }
 
-//Get dad joke, read dad joke
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  console.log("video id @ player creation: ", toString(globalVideoId))
+  player = new YT.Player('player', {
+    height: '400',
+    width: '400',
+    videoId: "BsY0Tazygqk",
+    paused: "true",
+    events: {
+      'onReady': onPlayerReady,
+    }
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady() {
+  console.log("Video Ready");
+}
+
+
+
+
+//Play / Pause button behavior 
+document.getElementById('playButton').onclick = function fun(playerStatus) {
+  // playerStatus attribute of player, -1 = not started,  1 = playing, 2 = paused.
+
+  // if player status is -1(not started) or 2 (paused), play the video
+  if (playerStatus == 2 || -1) {
+
+    player.playVideo();
+    console.log("Playing Video");
+
+    // otherwise if player is already playing, pause it. 
+  } else if (playerStatus == 1) {
+
+    player.pauseVideo();
+    console.log("Paused Video");
+
+  }
+}
+
+
+//Click Behavior for happy emoji
+document.getElementById('happy').onclick = function fun() {
+
+  videoQuery("happy");
+
+}
+
+
+//Get dad joke, backup functionality if API Key Burns
 function getDadJoke() {
   $.ajax({
     url: "https://icanhazdadjoke.com/",
