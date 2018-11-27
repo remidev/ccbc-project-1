b6dbbbc85ebe8a58a6908b4dd1abfb097e80ee99
lancep888 <lance.pu@sema4genomics.com>
2018-11-27 16:12:54 -0500

    move dadjoke to a seperate file, change speak voice to US English Male

--- assets/js/video.js
+++ assets/js/video.js
@@ -172,16 +172,3 @@ document.getElementById('sample').onclick = function fun() {
   //https://www.youtube.com/watch?v=YrvlVLLabro&list=RDYrvlVLLabro&start_radio=1
 }
 
-//Get dad joke, backup functionality if API Key Burns
-function getDadJoke() {
-  $.ajax({
-    url: "https://icanhazdadjoke.com/",
-    type: "GET",
-    headers: {
-      Accept: 'application/json'
-    },
-  }).then(function (response) {
-    console.log(response);
-    responsiveVoice.speak(response.joke)
-  })
-}
