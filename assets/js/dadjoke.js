b9d54f6a44135244530357ef84edda296255c147
remidev <28935481+remidev@users.noreply.github.com>
2018-11-28 20:28:57 -0500

    added more comments & improved README

--- assets/js/dadjoke.js
+++ assets/js/dadjoke.js
@@ -1,13 +1,21 @@
 //Get dad joke, backup functionality if API Key Burns
 function getDadJoke() {
-    $.ajax({
-      url: "https://icanhazdadjoke.com/",
-      type: "GET",
-      headers: {
-        Accept: 'application/json'
-      },
-    }).then(function (response) {
-      console.log(response);
-      responsiveVoice.speak(response.joke, "US English Male")
-    })
-  }
+
+  $.ajax({
+
+    //Dad joke API URL
+    url: "https://icanhazdadjoke.com/",
+    type: "GET",
+    headers: {
+
+      Accept: 'application/json'
+
+    },
+
+  }).then(function (response) {
+
+    //Play response.joke in selected voice
+    responsiveVoice.speak(response.joke, "US English Male")
+
+  })
+}
